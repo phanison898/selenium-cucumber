9e6892b81462c80b44bcb7fdb8eaf3660d77812c
phanison898 <phanison898@gmail.com>
2024-02-29 00:39:47 +0530

    added runner & extent reports

--- src/test/java/com/runners/LoginRunner.java
+++ src/test/java/com/runners/LoginRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = { "classpath:features" }, glue = { "classpath:com.stepDefinitions",
-		"classpath:com.helpers" })
+		"classpath:com.helpers" }, plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 public class LoginRunner extends AbstractTestNGCucumberTests {
 
 }
