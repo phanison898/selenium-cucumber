59b47b7487e66ed9b7bcc473a33487f12fe5e945
phanison898 <phanison898@gmail.com>
2024-02-29 16:44:53 +0530

    added NoSuchBrowser custom exception

---
+++ src/main/java/com/custom/exceptions/NoSuchBrowserException.java
@@ -0,0 +1,14 @@
+package com.custom.exceptions;
+
+public class NoSuchBrowserException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoSuchBrowserException(String message) {
+		super(message);
+	}
+
+}
--- src/test/java/com/helpers/Hooks.java
+++ src/test/java/com/helpers/Hooks.java
@@ -8,6 +8,7 @@ import com.base.Base;
 import com.browsers.ChromeBrowser;
 import com.browsers.EdgeBrowser;
 import com.browsers.FirefoxBrowser;
+import com.custom.exceptions.NoSuchBrowserException;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -19,6 +20,33 @@ public class Hooks extends Base {
 
 	@Before
 	public void setup(Scenario s) {
+		
+		try {
+			selectBrowser();
+		} catch (NoSuchBrowserException e) {
+			e.printStackTrace();
+		}
+
+		getDriver().get(Config.getURL());
+
+	}
+
+	@After
+	public void tearDown(Scenario scenario) {
+
+		if (scenario.isFailed()) {
+
+			byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+
+			scenario.attach(screenshot, "image/png", "screenshot");
+
+		}
+
+		getDriver().close();
+		getDriver().quit();
+	}
+
+	private void selectBrowser() throws NoSuchBrowserException {
 
 		String browser = Config.getBrowser();
 
@@ -40,31 +68,13 @@ public class Hooks extends Base {
 			break;
 
 		default:
-			System.out.println(String
-					.format("Selected browser [ %s ] is not available. Launching default chrome browser", browser));
-			ChromeBrowser _cb = new ChromeBrowser();
-			setDriver(_cb.init());
-			break;
+
+			String message = String.format(
+					"Selected browser [ %s ] isn't available. Select any one of these browsers { 'chrome', 'firefox', 'edge' }",
+					browser);
+			throw new NoSuchBrowserException(message);
 
 		}
-
-		getDriver().get(Config.getURL());
-
-	}
-
-	@After
-	public void tearDown(Scenario scenario) {
-
-		if (scenario.isFailed()) {
-
-			byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
-
-			scenario.attach(screenshot, "image/png", "screenshot");
-
-		}
-
-		getDriver().close();
-		getDriver().quit();
 	}
 
 }
